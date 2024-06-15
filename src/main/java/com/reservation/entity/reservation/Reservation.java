4cb6ab95499f7be4e71bf13bb6530ea09615a5f6
somsomi3 <chfhddlfhqls@naver.com>
2024-06-15 23:50:19 +0900

    chore: Store-> Restaurant로 파일명 변경

--- src/main/java/com/reservation/entity/reservation/Reservation.java
+++ src/main/java/com/reservation/entity/reservation/Reservation.java
@@ -15,16 +15,16 @@ import java.time.LocalDateTime;
 public class Reservation {
     @Id
     @GeneratedValue
-    @Column(name = "reservation_id")
+    @Column(name = "rsv_id")
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne
     @JoinColumn(name = "store_id")
-    private Store store;
+    private Restaurant store;
 
 //    @OneToOne
 //    @JoinColumn(name = "review_id")
--- src/main/java/com/reservation/entity/reservation/Restaurant.java
+++ src/main/java/com/reservation/entity/reservation/Restaurant.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 @Builder
-public class Store {
+public class Restaurant {
     @Id
     @GeneratedValue
     @Column(name = "store_id")
