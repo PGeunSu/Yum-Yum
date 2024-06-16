0412760e2a8b4c75d0984a360ad8dc4cc042990a
rlj_su <geunsu902@gmail.com>
2024-06-16 17:31:11 +0900

    cache

--- src/main/java/com/reservation/config/FeignConfig.java
+++ src/main/java/com/reservation/config/FeignConfig.java
@@ -2,22 +2,23 @@ package com.reservation.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ImportAutoConfiguration({FeignAutoConfiguration.class})
+//@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class FeignConfig {
 
-//  @Value(value = "${mailgun.key}")
-//  private String mailgunKey;
+  @Value(value = "${spring.mailgun.key}")
+  private String mailgunKey;
 
   @Qualifier(value = "mailgun")
   @Bean
   public BasicAuthRequestInterceptor basicAuthRequestInterceptor(){
-    return new BasicAuthRequestInterceptor("api","134f7f536b270aeaf2f91505959f42e1-51356527-087aa56c");
+    return new BasicAuthRequestInterceptor("api", mailgunKey);
   }
 
 
