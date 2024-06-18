3cdf4fb85813463cd8072a28f097da9329d56a08
somsomi3 <chfhddlfhqls@naver.com>
2024-06-18 21:06:43 +0900

    chore : 함수명 수정2

--- src/main/java/com/reservation/dto/reservation/RestaurantList.java
+++ src/main/java/com/reservation/dto/reservation/RestaurantList.java
@@ -10,16 +10,16 @@ public class RestaurantList {
     private Long id;
     private String post;
     private String address;
-    private String newadress;
+    private String newAdress;
     private String tell;
     private String time;
 
     public void RestaurantList(Long id, String post, String address,
-        String newadress, String tell, String time) {
+        String newAdress, String tell, String time) {
         this.id = id;
         this.post = post;
         this.address = address;
-        this.newadress = newadress;
+        this.newAdress = newAdress;
         this.tell = tell;
         this.time = time;
     }
--- src/main/java/com/reservation/entity/reservation/Restaurant.java
+++ src/main/java/com/reservation/entity/reservation/Restaurant.java
@@ -23,16 +23,16 @@ public class Restaurant {
     private Long id;
     private String post;
     private String address;
-    private String newadress;
+    private String newAdress;
     private String tell;
     private String time;
 
     public void RestaurantList(Long id, String post, String address,
-        String newadress, String tell, String time) {
+        String newAdress, String tell, String time) {
         this.id = id;
         this.post = post;
         this.address = address;
-        this.newadress = newadress;
+        this.newAdress = newAdress;
         this.tell = tell;
         this.time = time;
     }
