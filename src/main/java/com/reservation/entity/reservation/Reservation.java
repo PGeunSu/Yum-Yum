b8a69c1ac5bd4f8f2a55746a16728101d4dcf148
somsomi3 <chfhddlfhqls@naver.com>
2024-06-18 12:27:38 +0900

    fix: Reservation entity 수정

--- src/main/java/com/reservation/entity/reservation/Reservation.java
+++ src/main/java/com/reservation/entity/reservation/Reservation.java
@@ -1,12 +1,24 @@
 package com.reservation.entity.reservation;
+
 import com.reservation.entity.user.User;
 import com.reservation.type.ReservationStatus;
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity
+@Entity(name = "reservation")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -14,26 +26,21 @@ import java.time.LocalDateTime;
 @Builder
 public class Reservation {
     @Id
-    @GeneratedValue
-//    @Column(name = "rsv_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rsv_id")
     private Long id;
 
     @ManyToOne
-//    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id")//이름 논의 필요
     private User user;
 
     @ManyToOne
-//    @JoinColumn(name = "res_id")
+    @JoinColumn(name = "res_id")//이름 논의 필요
     private Restaurant restaurant;
 
     private LocalDateTime time;
     private String place;
     private String name;
-
-//    @OneToOne
-//    @JoinColumn(name = "review_id")
-//    @JsonManagedReference
-//    private Review review;
 
     @Enumerated(EnumType.STRING)
     private ReservationStatus reservationStatus;
