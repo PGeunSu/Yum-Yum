78fe43eb15d001c894f15c4c9f512617ce3a3a26
gilhyunLee <rlfgus917@naver.com>
2024-06-18 19:15:58 +0900

    feat : 보드 카테고리 추가

---
+++ src/main/java/com/reservation/enum_class/BoardCategory.java
@@ -0,0 +1,12 @@
+package com.reservation.enum_class;
+
+public enum BoardCategory {
+  A, B, C;
+
+  public static BoardCategory of(String category) {
+    if (category.equalsIgnoreCase("free")) return BoardCategory.A;
+    else if (category.equalsIgnoreCase("greeting")) return BoardCategory.B;
+    else if (category.equalsIgnoreCase("gold")) return BoardCategory.C;
+    else return null;
+  }
+}
