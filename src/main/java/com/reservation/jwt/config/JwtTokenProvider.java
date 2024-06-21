e5602e168f5d74fe5bd01bc16fe26346e6242171
rlj_su <geunsu902@gmail.com>
2024-06-21 21:13:46 +0900

    fix : jwt token 코드 수정

--- src/main/java/com/reservation/jwt/config/JwtTokenProvider.java
+++ src/main/java/com/reservation/jwt/config/JwtTokenProvider.java
@@ -1,10 +1,13 @@
 package com.reservation.jwt.config;
 
-import com.reservation.jwt.dto.JwtDto;
 import com.reservation.jwt.dto.UserVo;
 import com.reservation.jwt.filter.Aes256Util;
+import com.reservation.service.UserService;
+import com.reservation.service.VerifyService;
+import com.reservation.type.UserType;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -17,6 +20,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -29,112 +33,23 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class JwtTokenProvider {
 
-  private final Key key;
+  private final VerifyService verifyService;
+
+  @Value("${spring.jwt.secret}")
+  private String key;
 
   private long tokenValidTime = 1000L * 60 * 60 * 24;
 
-  //application.yml 에서 secret 값 가져오기
-  public JwtTokenProvider(@Value("${spring.jwt.secret}") String secretKey) {
-    byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-    this.key = Keys.hmacShaKeyFor(keyBytes);
-  }
-
-  public JwtDto generateToken(Authentication authentication) {
-    //권한 가져오기
-    String authorities = authentication.getAuthorities().stream()
-        .map(GrantedAuthority::getAuthority)
-        .collect(Collectors.joining(","));
-
-    long now = (new Date()).getTime();
-
-    //Access Token 생성
-    Date accessTokenExpiresln = new Date(now + 86400000);
-
-    String accessToken = Jwts.builder()
-        .setSubject(authentication.getName())
-        .claim("auth", authorities)
-        .setExpiration(accessTokenExpiresln)
-        .signWith(key, SignatureAlgorithm.HS256)
-        .compact();
-
-    //Refresh Token 생성
-    String refreshToken = Jwts.builder()
-        .setExpiration(new Date(now + 86400000))
-        .signWith(key, SignatureAlgorithm.HS256)
-        .compact();
-
-    return JwtDto.builder()
-        .grantType("Bearer")
-        .accessToken(accessToken)
-        .refreshToken(refreshToken)
-        .build();
-  }
-
-  //Jwt 토큰 정보 추출
-  public Authentication getAuthentication(String accessToken) {
-    //Jwt 복호화
-    Claims claims = parseClaims(accessToken);
-
-    if (claims.get("auth") == null) {
-      throw new RuntimeException("권한 정보가 없는 토큰입니다.");
-    }
-
-    //권한 정보 가져오기
-    Collection<? extends GrantedAuthority> authorities = Arrays.stream(
-            claims.get("auth").toString().split(","))
-        .map(SimpleGrantedAuthority::new)
-        .collect(Collectors.toList());
-    UserDetails principal = new User(claims.getSubject(), "", authorities);
-
-    return new UsernamePasswordAuthenticationToken(principal, "", authorities);
-  }
-
-  //토큰에서 사용자 정보 추출
-  public UserVo getUserVo(String token) {
-    Claims c = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
-
-    return new UserVo(
-        Long.valueOf(Objects.requireNonNull(Aes256Util.decrypt(c.getId()))),
-        Aes256Util.decrypt(c.getSubject()),
-        Aes256Util.decrypt(c.getAudience()));
-  }
-
-  private Claims parseClaims(String accessToken) {
-    try {
-      return Jwts.parserBuilder()
-          .setSigningKey(key)
-          .build()
-          .parseClaimsJws(accessToken)
-          .getBody();
-    } catch (ExpiredJwtException e) {
-      return e.getClaims();
-    }
-  }
-
-  //토큰 정보 검증
-  public boolean validateToken(String token) {
-    try {
-      Jwts.parserBuilder()
-          .setSigningKey(key)
-          .build()
-          .parseClaimsJws(token);
-    } catch (SecurityException | MalformedJwtException e) {
-      log.info("Invalid JWT Token", e);
-    } catch (ExpiredJwtException e) {
-      log.info("Expired JWT Token", e);
-    } catch (UnsupportedJwtException e) {
-      log.info("Unsupported JWT Token", e);
-    } catch (IllegalArgumentException e) {
-      log.info("JWT claims string is empty", e);
-    }
-    return false;
-  }
-
-  public String createToken(String userPk, Long id) {
-    Claims claims = Jwts.claims().setSubject(Aes256Util.encrypt(userPk))
+  public String createToken(String name, Long id, UserType userType, String email) {
+    Claims claims = Jwts.claims()
+        .setSubject(Aes256Util.encrypt(name))
+        .setAudience(Aes256Util.encrypt(email))
         .setId(Aes256Util.encrypt(id.toString()));
+    claims.put("role",userType);
+
     Date now = new Date();
 
     return Jwts.builder()
@@ -144,4 +59,58 @@ public class JwtTokenProvider {
         .signWith(SignatureAlgorithm.HS256, key)
         .compact();
   }
+
+  //토큰 정보 검증
+  public boolean validateToken(String token) {
+    try {
+      Jws<Claims> claimsJws = Jwts.parser().setSigningKey(key).parseClaimsJws(token);
+      return !claimsJws.getBody().getExpiration().before(new Date());
+    } catch (Exception e) {
+      return false;
+    }
+  }
+
+  //토큰에서 사용자 정보 추출
+  public UserVo getUserVo(String token) {
+    Claims c = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+
+    return new UserVo(
+        Long.valueOf(Objects.requireNonNull(Aes256Util.decrypt(c.getId()))),
+        Aes256Util.decrypt(c.getSubject()),
+        Aes256Util.decrypt(c.getAudience()),
+        String.valueOf(c.get("role"))
+    );
+  }
+
+
+  private Claims parseClaims(String accessToken) {
+    try {
+      return Jwts.parser()
+          .setSigningKey(key)
+          .parseClaimsJws(accessToken).getBody();
+    } catch (ExpiredJwtException e) {
+      return e.getClaims();
+    }
+  }
+
+  // 토큰을 파싱해서 토큰안 이름 정보 가져오기
+  public String getUsername(String token) {
+    return this.parseClaims(token).getSubject();
+  }
+
+  //Jwt 토큰 정보 추출
+  public Authentication getAuthentication(String token) {
+    UserDetails userDetails = verifyService.loadUserByUsername(getUsername(token));
+
+    return new UsernamePasswordAuthenticationToken(
+        userDetails, "", userDetails.getAuthorities()
+    );
+  }
+
+
+
+
+
+
+
 }
--- src/main/java/com/reservation/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/reservation/jwt/filter/JwtAuthenticationFilter.java
@@ -1,26 +1,30 @@
 package com.reservation.jwt.filter;
 
 import com.reservation.jwt.config.JwtTokenProvider;
+import io.micrometer.common.lang.NonNullApi;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   public static final String TOKEN_HEADER = "Authorization";
   public static final String TOKEN_PREFIX = "Bearer ";
 
-  private final JwtTokenProvider jwtTokenProvider;
+  private final JwtTokenProvider provider;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
@@ -30,21 +34,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     String token = resolveToken(request);
 
     //유효성 검사
-    if (token != null && jwtTokenProvider.validateToken(token)){
+//    if (StringUtils.hasText(token) && provider.validateToken(token) && provider.getUserVo(token)
+//        .getUserType().equals("USER"))
+
+      if (StringUtils.hasText(token) && provider.validateToken(token)){
       //유효할 경우 객체를 가지고와서 SecurityContext 에 저장
-      Authentication authentication = jwtTokenProvider.getAuthentication(token);
+      Authentication authentication = provider.getAuthentication(token);
       SecurityContextHolder.getContext().setAuthentication(authentication);
     }
     chain.doFilter(request, response);
   }
 
   private String resolveToken(HttpServletRequest request){
-    String bearerToken = request.getHeader(TOKEN_HEADER);
-    if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(TOKEN_PREFIX)){
-      return bearerToken.substring(TOKEN_PREFIX.length());
+    String token = request.getHeader(TOKEN_HEADER);
+
+    System.out.println(token);
+
+    if (!ObjectUtils.isEmpty(token) && token.startsWith(TOKEN_PREFIX)) {
+      return token.substring(TOKEN_PREFIX.length());
     }
 
     return null;
-
   }
 }
