1016c3d8c9a7513be355a53ec70dabe64dc34127
rlj_su <geunsu902@gmail.com>
2024-06-26 21:39:27 +0900

    feat : front 부분 추가

--- src/main/java/com/reservation/config/SecurityConfig.java
+++ src/main/java/com/reservation/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.reservation.config;
 
 import com.reservation.jwt.config.JwtTokenProvider;
 import com.reservation.jwt.filter.JwtAuthenticationFilter;
+import com.reservation.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +15,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -22,16 +25,30 @@ public class SecurityConfig {
   private final JwtTokenProvider jwtTokenProvider;
 
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        .and()
+//        .logout() //로그아웃 url
+//        .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+//        .logoutSuccessUrl("/"); //로그아웃 성공 시 이동할 url
     http
         .httpBasic(AbstractHttpConfigurer::disable)
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests((auth) ->
-            auth.requestMatchers("/users/**","/swagger-ui/**","/v3/**","/myHandler/**",
-                "/boards/**","/comments/**","/messages/**","/reservation/**","/restaurant/**","/likes/**").permitAll() //해당 API 의 요청 허가
+            auth
+                .requestMatchers("/css/**", "/js/**", "/img/**", "/users/**", "/swagger-ui/**",
+                    "/v3/**", "/myHandler/**",
+                    "/boards/**", "/comments/**", "/messages/**", "/reservation/**",
+                    "/restaurant/**", "/likes/**").permitAll() //해당 API 의 요청 허가
                 .anyRequest().authenticated()) //이 밖에 모든 요청은 인증 필요
-        .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-        .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+            UsernamePasswordAuthenticationFilter.class)
+        .sessionManagement(
+            (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .formLogin((formLogin) -> formLogin
+        .loginPage("/users/signIn") //로그인 페이지 url 설정
+        .defaultSuccessUrl("/main") //로그인 성공 시 해당 url 반환
+        .usernameParameter("email") //로그인 시 사용할 파라미터 이름으로 email 설정
+        .failureUrl("/users/login/error")); //로그인 실패 시 이동할 Url
     return http.build();
   }
 
@@ -40,7 +57,6 @@ public class SecurityConfig {
     // BCrypt Encoder 사용
     return PasswordEncoderFactories.createDelegatingPasswordEncoder();
   }
-
 
 
 }
--- src/main/java/com/reservation/controller/UserController.java
+++ src/main/java/com/reservation/controller/UserController.java
@@ -2,51 +2,83 @@ package com.reservation.controller;
 
 import com.reservation.dto.user.SignInForm;
 import com.reservation.dto.user.SignUpForm;
-import com.reservation.dto.user.UserDto;
 import com.reservation.dto.user.UserModifiedDto;
-import com.reservation.entity.user.User;
-import com.reservation.jwt.dto.TokenDto;
 import com.reservation.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
   private final UserService  userService;
 
-  //회원가입
-  @PostMapping("/signUp")
-  public ResponseEntity<UserDto> signUp(@RequestBody SignUpForm form){
-    return ResponseEntity.ok(userService.userSignUp(form));
+
+  @GetMapping("/signUp")
+  public String signUp(Model model){
+    model.addAttribute("signUpForm", new SignUpForm());
+    return "user/signUp";
   }
 
-  @GetMapping("/Test")
-  public Long test(@AuthenticationPrincipal TokenDto dto){
-    return dto.getId();
+  //회원가입
+  @PostMapping("/signUp")
+  public String signUp(@Valid SignUpForm form, BindingResult bindingResult, Model model){
+
+    if(bindingResult.hasErrors()){
+      return "user/signUp";
+    }
+    try{
+      userService.userSignUp(form);
+    }catch (IllegalStateException e){
+      model.addAttribute("errorMessage", e.getMessage());
+      return "user/signUp";
+    }
+    //회원가입 성공시 이메일 인증
+    return "user/verify";
+  }
+
+  //메인화면
+  @GetMapping("/main")
+  public String mainPage(){
+    return "home";
   }
 
   //회원가입 인증
   @GetMapping("/verify")
-  public ResponseEntity<String> verifyUser(String email, String code){
-    userService.verifyEmail(email, code);
-    return ResponseEntity.ok("인증이 완료되었습니다");
+  public String verifyUser(String email, String code, Model model){
+    try{
+      userService.verifyEmail(email, code);
+    }catch (IllegalStateException e){
+      model.addAttribute("errorMessage", e.getMessage());
+      return "user/verify";
+    }
+
+    return "redirect:/";
   }
 
   //로그인
-  @PostMapping("/signIn")
-  public ResponseEntity<String> signIn(@RequestBody SignInForm form){
-    return ResponseEntity.ok(userService.loginToken(form));
+  @GetMapping("/signIn")
+  public String signIn(@Valid SignInForm form){
+    userService.loginToken(form);
+    return "/user/signIn";
+  }
+
+  @GetMapping("/signIn/error")
+  public String loginError(Model model){
+    model.addAttribute("loginErrorMsg","아이디 또는 비밀번호를 확인해주세요");
+    return "/user/signIn";
   }
 
   //회원정보 수정
