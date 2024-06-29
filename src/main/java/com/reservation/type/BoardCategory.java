d1b6aa955da8c848633a85b42819b596a0eb012d
rlj_su <geunsu902@gmail.com>
2024-06-29 16:05:13 +0900

    refactor : board enum 이름변경

--- src/main/java/com/reservation/type/BoardCategory.java
+++ src/main/java/com/reservation/type/BoardCategory.java
@@ -1,15 +1,15 @@
 package com.reservation.type;
 
 public enum BoardCategory {
-  A, B, C;
+ FREE, GREETING, GOLD;
 
   public static BoardCategory of(String category) {
     if (category.equalsIgnoreCase("free")) {
-      return BoardCategory.A;
+      return BoardCategory.FREE;
     } else if (category.equalsIgnoreCase("greeting")) {
-      return BoardCategory.B;
+      return BoardCategory.GREETING;
     } else if (category.equalsIgnoreCase("gold")) {
-      return BoardCategory.C;
+      return BoardCategory.GOLD;
     } else {
       return null;
     }
