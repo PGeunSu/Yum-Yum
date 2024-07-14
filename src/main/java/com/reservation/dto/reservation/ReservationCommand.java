5563cd14e732518db0b8cec8bfdd785370371f33
rlj_su <geunsu902@gmail.com>
2024-07-14 16:24:29 +0900

    feat : 예약 ErrorCode, 프론트부분 추가

---
+++ src/main/java/com/reservation/dto/reservation/ReservationCommand.java
@@ -0,0 +1,57 @@
+package com.reservation.dto.reservation;
+
+import com.reservation.dto.reservation.util.TimeParsingUtils;
+import com.reservation.entity.reservation.Reservation;
+import com.reservation.entity.reservation.Restaurant;
+import com.reservation.entity.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+public class ReservationCommand {
+
+  @Getter
+  @Setter
+  @ToString
+  @NoArgsConstructor
+  public static class RegisterReservation {
+
+    private Long userId;
+    private String startTime;
+    private String endTime;
+
+    public Reservation toEntity(User user, Restaurant restaurant) {
+      return Reservation.builder()
+          .startTime(TimeParsingUtils.formatterLocalDateTime(startTime))
+          .endTime(TimeParsingUtils.formatterLocalDateTime(endTime))
+          .user(user)
+          .restaurant(restaurant)
+          .build();
+    }
+  }
+
+  @Getter
+  @Setter
+  @ToString
+  @NoArgsConstructor
+  public static class UpdateReservation {
+
+    private Long userId;
+    private Long restaurantId;
+    private String startTime;
+    private String endTime;
+
+    public Reservation toEntity(ValidateFindByIdDto validateFindByIdDto) {
+      return Reservation.builder()
+          .startTime(TimeParsingUtils.formatterLocalDateTime(startTime))
+          .endTime(TimeParsingUtils.formatterLocalDateTime(endTime))
+          .user(validateFindByIdDto.getUser())
+          .restaurant(validateFindByIdDto.getRestaurant())
+          .build();
+    }
+  }
+
+
+}
--- src/main/java/com/reservation/repository/ReservationRepository.java
+++ src/main/java/com/reservation/repository/ReservationRepository.java
@@ -1,13 +1,35 @@
 package com.reservation.repository;
 
 import com.reservation.entity.reservation.Reservation;
+import feign.Param;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     Page<Reservation> findByUserIdOrderByTimeDesc(Long userId, Pageable pageable);
 
+    @Query("select r from Reservation r left join fetch r.restaurant where" +
+        "r.restaurant.id = :restaurantId or r.startTime = :sTime or r.endTime = :eTime")
+    List<Reservation> findByRestaurantIdAndTime(
+        @Param("restaurantId") Long restaurantId,
+        @Param("sTime") LocalDateTime sTime,
+        @Param("sTime") LocalDateTime eTime);
+
+    @Query("select r from Reservation r left join fetch r.restaurant where r.restaurant.id = :restaurantId " +
+        "and r.startTime >= :sTime and r.endTime <= :eTime")
+    List<Reservation> findByRestaurantIdAndTimePeriod(
+        @Param("restaurantId") Long restaurantId,
+        @Param("sTime") LocalDateTime sTime,
+        @Param("sTime") LocalDateTime eTime);
+
+    List<Reservation> findByUserId(Long userId);
+
+    List<Reservation> findByRestaurantId(Long restaurantId);
 }
